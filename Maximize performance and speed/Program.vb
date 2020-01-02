148d0f8204bcc3333f67d1ce182890a70ce97503
estar73 <estarkov@gmail.com>
2020-01-02 04:53:29 +0000

    = regular update

--- Maximize performance and speed/Program.vb
+++ Maximize performance and speed/Program.vb
@@ -12,7 +12,7 @@
 
 Imports Bytescout.PDFExtractor
 
-' This example demonstrates the use of Optical Character Recognition (OCR) to extract text into xml
+' This example demonstrates the use of Optical Character Recognition (OCR) with "OCRMaximizeCPUUtilization" property to extract text
 ' from scanned PDF documents and raster images.
 
 ' To make OCR work you should add the following references to your project:
@@ -22,8 +22,8 @@ Class Program
 
     Friend Shared Sub Main(args As String())
 
-        ' Create Bytescout.PDFExtractor.XMLExtractor instance
-        Dim extractor As New XMLExtractor()
+        ' Create Bytescout.PDFExtractor.TextExtractor instance
+        Dim extractor As New TextExtractor()
         extractor.RegistrationName = "demo"
         extractor.RegistrationKey = "demo"
 
@@ -35,52 +35,26 @@ Class Program
         extractor.OCRMode = OCRMode.Auto
 
         ' Set the location of OCR language data files
-        extractor.OCRLanguageDataFolder = "c:\Program Files\Bytescout PDF Extractor SDK\ocrdata_best"
+        extractor.OCRLanguageDataFolder = "c:\Program Files\Bytescout PDF Extractor SDK\ocrdata_best\"
         
         ' Set OCR language
         extractor.OCRLanguage = "eng"  ' "eng" for english, "deu" for German, "fra" for French, "spa" for Spanish etc - according to files in "ocrdata" folder
         ' Find more language files at https://github.com/bytescout/ocrdata
-        
+
         ' Set PDF document rendering resolution
         extractor.OCRResolution = 300
 
-
-        ' You can also apply various preprocessing filters
-        ' to improve the recognition on low-quality scans.
-
-        ' Automatically deskew skewed scans
-        'extractor.OCRImagePreprocessingFilters.AddDeskew()
-
-        ' Remove vertical or horizontal lines (sometimes helps to avoid OCR engine's page segmentation errors)
-        'extractor.OCRImagePreprocessingFilters.AddVerticalLinesRemover()
-        'extractor.OCRImagePreprocessingFilters.AddHorizontalLinesRemover()
-
-        ' Repair broken letters
-        'extractor.OCRImagePreprocessingFilters.AddDilate()
-
-        ' Remove noise
-        'extractor.OCRImagePreprocessingFilters.AddMedian()
-
-        ' Apply Gamma Correction
-        'extractor.OCRImagePreprocessingFilters.AddGammaCorrection()
-
-        ' Add Contrast
-        'extractor.OCRImagePreprocessingFilters.AddContrast(20)
-
-
-        ' (!) You can use new OCRAnalyzer class to find an optimal set of image preprocessing 
-        ' filters for your specific document.
-        ' See "OCR Analyser" example.
-
+        ' Enables max use of CPU And max use of multiple threads during OCR
+        extractor.OCRMaximizeCPUUtilization = True
 
         ' Save extracted text to file
-        extractor.SaveXMLToFile("output.xml")
+        extractor.SaveTextToFile("output.txt")
 
         ' Cleanup
-        extractor.Dispose()
+		extractor.Dispose()
 
         ' Open output file in default associated application
-        System.Diagnostics.Process.Start("output.xml")
+        System.Diagnostics.Process.Start("output.txt")
 
     End Sub
     
